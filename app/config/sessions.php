8c742e8cbe988d190e97cd4264814b22e78121e9
Luke Watts <luke@luke-watts.com>
2015-03-06 16:57:30 +0000

    Adding session config file to app/config

---
+++ app/config/sessions.php
@@ -0,0 +1,20 @@
+<?php
+session_start();
+
+/**
+ * Session configuration settings
+ *
+ * @package Thistle
+ * @since 3.3.0
+ */
+
+$GLOBALS['config'] = array(
+  'cookie' => array(
+    'name' => 'hash',
+    'expiry' => 604800
+  ),
+  'session' => array(
+    'name' => 'user',
+    'token' => 'token',
+  )
+);
