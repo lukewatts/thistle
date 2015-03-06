ba9ff47712ea2b2c9b6a483500ab7704d89802ad
Luke Watts <luke@luke-watts.com>
2015-03-06 16:59:34 +0000

    Adding Config class to deal with config arrays

---
+++ app/libs/Config.php
@@ -0,0 +1,24 @@
+<?php
+
+class Config {
+
+  public static function get( $path = null ) {
+    if ( $path ) {
+
+      $config = $GLOBALS['config'];
+      $key = explode( '.', $path );
+
+      foreach( $key as $val ) {
+
+        if ( isset( $config[$val] ) ) {
+          $config = $config[$val];
+        }
+
+      }
+
+      return $config;
+
+    }
+  }
+
+}
