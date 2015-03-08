3e1401352065b5907729747548da2a415babb793
Luke Watts <luke@luke-watts.com>
2015-03-08 18:41:10 +0000

    Adding Session::flash() method

--- app/libs/Session.php
+++ app/libs/Session.php
@@ -1,11 +1,75 @@
 <?php
-
+/**
+ * Class for setting, getting, checking and deleting sessions
+ *
+ * @package Thistle
+ * @since  3.3.0
+ * 
+ */
 class Session {
 
+  /**
+   * Set a session by name => value and return it
+   * 
+   * @param  string $name
+   * @param  string $value
+   * @return string
+   */
   public static function set( $name, $value ) {
     $session = $_SESSION[$name] = $value;
 
     return $session;
   }
-  
+
+  /**
+   * Get session by name
+   * 
+   * @param  string $name
+   * @return string
+   */
+  public function get( $name ) {
+    return $_SESSION[$name];
+  }
+
+  /**
+   * Check if a session exists by name
+   * 
+   * @param  string $name
+   * @return boolean
+   */
+  public static function exists( $name ) {
+    return ( isset( $_SESSION[$name] ) ) ? true : false;
+  }
+
+  /**
+   * Delete a session by name
+   * 
+   * @param  string $name
+   * @return void
+   */
+  public static function delete( $name ) {
+    if ( self::exists( $name ) ) {
+      unset( $_SESSION[$name] );
+    }
+  }
+
+  /**
+   * Flash a message to the user after a session is created
+   * 
+   * @param  string $name
+   * @param  string $message
+   * @return mixed
+   */
+  public static function flash( $name, $message = '' ) {
+
+    if ( self::exists( $name ) ) {
+      $session = self::get( $name );
+      self::delete( $name );
+      return $session;
+    }
+    else {
+      self::set( $name, $message );
+    }
+  }
+
 }
--- app/libs/Token.php
+++ app/libs/Token.php
@@ -1,11 +1,41 @@
 <?php
 
+/**
+ * Creating and checking tokens
+ */
 class Token {
   
+  /**
+   * Generate a unique token from the existing session
+   * 
+   * @return string
+   * @since  3.3.0
+   */
   public static function make() {
     
     $token = Session::set( Config::get( 'session.token' ), md5( uniqid() ) );
-    
+
     return $token;
   }
+
+  /**
+   * Check if the token matches the session
+   * 
+   * @param  string $token
+   * @return boolean
+   */
+  public static function check( $token ) {
+    $token_name = Config::get( 'session.token' );
+
+    if ( Session::exists( $token_name ) && $token === Session::get( $token_name ) ) {
+      
+      Session::delete( $token_name );
+      
+      return true;
+    }
+
+    return false;
+
+  }
+
 }
