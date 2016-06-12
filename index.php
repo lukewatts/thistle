1309c4efce455b60d945c75671af1224ff03e72e
Luke Watts <luke@luke-watts.com>
2016-06-12 14:40:57 +0100

    Adding comment to index.php explaing Thistle and showing version

--- index.php
+++ index.php
@@ -1,2 +1,18 @@
 <?php
+
+/**
+ * ------------------------------------------------------------
+ * Thistle Framework
+ * ------------------------------------------------------------
+ *
+ * The Thistle Framework is built on the Silex framework v1.3
+ * http://silex.sensiolabs.org/doc/1.3. However, it comes
+ * with Controllers, Models, Views, a config file and
+ * routes already set up to use immediately.
+ *
+ * @author  Luke Watts <luke@affinity4.ie>
+ * @since   0.0.1
+ *
+ * @version 0.0.3
+ */
 require_once __DIR__ . '/app/core/init.php';
