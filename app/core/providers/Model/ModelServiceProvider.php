bd001af74dd3419e5a3a0212ff7f8152cf9a76f6
Luke Watts <luke@luke-watts.com>
2016-06-13 19:32:18 +0100

    Readding Model without submodule!

---
+++ app/core/providers/Model/ModelServiceProvider.php
@@ -0,0 +1,22 @@
+<?php
+namespace App\Core\Provider\Model;
+
+use Silex\ServiceProviderInterface;
+use Silex\Application;
+
+class ModelServiceProvider implements ServiceProviderInterface
+{
+    public function register(Application $app)
+    {
+        // 
+    }
+
+    public function boot(Application $app)
+    {
+        foreach ($app['models'] as $label => $class) {
+            $app[$label] = $app->share(function($app) use ($class) {
+                return new $class($app['db']); 
+            });
+        }
+    }
+}
